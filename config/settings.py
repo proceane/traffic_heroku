26ea4d264348a3070ae93ff7cb414d7769d3199a
proceane <proceane1@gmail.com>
2021-07-14 21:43:41 +0900

    feat : static file setting

--- config/settings.py
+++ config/settings.py
@@ -124,7 +124,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_DIRS = [
+    STATIC_DIR,
+]
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
